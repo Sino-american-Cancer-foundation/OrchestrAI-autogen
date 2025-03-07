439ae843ef14d90373424847b0bbf797cbf9c076
phil-aid <phil.wu@aidtech.ai>
2025-03-07 21:00:24 +0000

    feat: add probill package with initial configuration, README, and tests; update dependencies and remove obsolete database files

--- python/packages/autogen-ext/src/autogen_ext/agents/probill/_probill_plan_agent.py
+++ python/packages/autogen-ext/src/autogen_ext/agents/probill/_probill_plan_agent.py
@@ -1,17 +1,49 @@
-from typing import Any
+from typing import (
+    Any,
+    AsyncGenerator,
+    Awaitable,
+    Callable,
+    Dict,
+    List,
+    Mapping,
+    Optional,
+    Sequence,
+    Tuple,
+    Union,
+)
 from autogen_core.models import ChatCompletionClient
 from autogen_agentchat.agents import AssistantAgent
 
 from dataclasses import dataclass
 from typing import Any, Callable, List, Literal
 from autogen_ext.models.ollama import OllamaChatCompletionClient
-from autogen_core import AgentId, MessageContext, RoutedAgent, SingleThreadedAgentRuntime, message_handler
+from autogen_core import AgentId, MessageContext, RoutedAgent, SingleThreadedAgentRuntime, AgentRuntime, Component, ComponentModel
 from azure.identity import DefaultAzureCredential, get_bearer_token_provider
 from langchain_core.messages import HumanMessage, SystemMessage
 from langchain_core.tools import tool  # pyright: ignore
 from langchain_ollama import ChatOllama
 from langgraph.graph import END, MessagesState, StateGraph
 from langgraph.prebuilt import ToolNode
+from pydantic import BaseModel
+from typing_extensions import Self
+from autogen_agentchat.base import ChatAgent, TerminationCondition, Handoff as HandoffBase
+from autogen_core.memory import Memory
+from autogen_core.tools import BaseTool, FunctionTool
+from autogen_core.models import (
+    AssistantMessage,
+    ChatCompletionClient,
+    CreateResult,
+    FunctionExecutionResult,
+    FunctionExecutionResultMessage,
+    LLMMessage,
+    ModelFamily,
+    SystemMessage,
+    UserMessage,
+)
+from autogen_core.model_context import (
+    ChatCompletionContext,
+    UnboundedChatCompletionContext,
+)
 
 MAGENTIC_ONE_CODER_DESCRIPTION = "A helpful and general-purpose AI assistant that has strong language skills, Python skills, and Linux command line skills."
 
@@ -26,25 +58,95 @@ Don't include multiple code blocks in one response. Do not ask users to copy and
 If the result indicates there is an error, fix the error and output the code again. Suggest the full code instead of partial code or code changes. If the error can't be fixed or if the task is not solved even after the code is executed successfully, analyze the problem, revisit your assumption, collect additional info you need, and think of a different approach to try.
 When you find an answer, verify the answer carefully. Include verifiable evidence in your response if possible."""
 
+class ProbillPlanAgentConfig(BaseModel):
+    """The declarative configuration for a ProbillPlanAgent."""
+    name: str
+    model_client: ComponentModel
+    tools: List[ComponentModel] | None
+    handoffs: List[HandoffBase | str] | None = None
+    model_context: ComponentModel | None = None
+    memory: List[ComponentModel] | None = None
+    description: str
+    system_message: str | None = None
+    model_client_stream: bool = False
+    reflect_on_tool_use: bool
+    tool_call_summary_format: str
 
-class ProbillPlanAgent(AssistantAgent):
+class ProbillPlanAgent(AssistantAgent,  Component[ProbillPlanAgentConfig]):
     """An agent, used by Probill that provides plaaning service.
 
     The prompts and description are sealed, to replicate the original MagenticOne configuration. See AssistantAgent if you wish to modify these values.
     """
 
     component_provider_override = "autogen_ext.agents.probill.ProbillPlanAgent"
-
+    component_config_schema = ProbillPlanAgentConfig
 
     def __init__(
         self,
         name: str,
         model_client: ChatCompletionClient,
+        *,
+        tools: List[BaseTool[Any, Any] | Callable[..., Any] | Callable[..., Awaitable[Any]]] | None = None,
+        handoffs: List[HandoffBase | str] | None = None,
+        model_context: ChatCompletionContext | None = None,
+        description: str = "An agent that provides assistance with ability to use tools.",
+        system_message: (
+            str | None
+        ) = "You are a helpful AI assistant. Solve tasks using your tools. Reply with TERMINATE when the task has been completed.",
+        model_client_stream: bool = False,
+        reflect_on_tool_use: bool = False,
+        tool_call_summary_format: str = "{result}",
+        memory: Sequence[Memory] | None = None,     
         **kwargs: Any,
     ):
+        self.parameters = kwargs if kwargs else {}
         super().__init__(
-            name,
-            model_client,
-            description=MAGENTIC_ONE_CODER_DESCRIPTION,
-            system_message=MAGENTIC_ONE_CODER_SYSTEM_MESSAGE,
+            name=name, 
+            description=description,
+            model_client=model_client,
+            tools=tools,
+            handoffs=handoffs,
+            model_context=model_context,
+            system_message=system_message,
+            model_client_stream=model_client_stream,
+            reflect_on_tool_use=reflect_on_tool_use,
+            tool_call_summary_format=tool_call_summary_format,
+            memory=memory,     
         )
+
+    def _to_config(self) -> ProbillPlanAgentConfig:
+        """Convert the assistant agent to a declarative config."""
+
+        return ProbillPlanAgentConfig(
+            name=self.name,
+            model_client=self._model_client.dump_component(),
+            tools=[tool.dump_component() for tool in self._tools],
+            handoffs=list(self._handoffs.values()) if self._handoffs else None,
+            model_context=self._model_context.dump_component(),
+            memory=[memory.dump_component() for memory in self._memory] if self._memory else None,
+            description=self.description,
+            system_message=self._system_messages[0].content
+            if self._system_messages and isinstance(self._system_messages[0].content, str)
+            else None,
+            model_client_stream=self._model_client_stream,
+            reflect_on_tool_use=self._reflect_on_tool_use,
+            tool_call_summary_format=self._tool_call_summary_format,
+        )
+
+    @classmethod
+    def _from_config(cls, config: ProbillPlanAgentConfig) -> Self:
+        """Create an assistant agent from a declarative config."""
+        return cls(
+            name=config.name,
+            model_client=ChatCompletionClient.load_component(config.model_client),
+            tools=[BaseTool.load_component(tool) for tool in config.tools] if config.tools else None,
+            handoffs=config.handoffs,
+            model_context=None,
+            memory=[Memory.load_component(memory) for memory in config.memory] if config.memory else None,
+            description=config.description,
+            system_message=config.system_message,
+            model_client_stream=config.model_client_stream,
+            reflect_on_tool_use=config.reflect_on_tool_use,
+            tool_call_summary_format=config.tool_call_summary_format,
+        )
+
--- python/packages/autogen-studio/autogenstudio/mcp_tools/__init__.py
+++ python/packages/autogen-studio/autogenstudio/mcp_tools/__init__.py
@@ -1,7 +1,5 @@
 # from .weather.src import server as weather_mcp_tool
-from .probill.src import server
-__all__ = [
-    # "weather_mcp_tool",
-    "server"
-]
+from .probill.src import server as ProbillMCPServer
+
+__all__ = ["ProbillMCPServer"]
 
